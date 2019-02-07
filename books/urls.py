69d7ac872bbc793025eec77005aafb64fe0d9c15
mohitjangra <99mohitjangra@gmail.com>
2019-02-07 16:47:18 +0530

    IIJI

--- books/urls.py
+++ books/urls.py
@@ -18,8 +18,12 @@ from django.conf.urls import url
 from . import views
 app_name = 'books'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    url(r'^$', views.first_page.as_view(), name='first_page'),
+    url(r'^books/$', views.IndexView.as_view(), name='index'),
+    url(r'^signup/$', views.UserFormView.as_view(), name='signup'),
+    url(r'^login/$', views.login_user.as_view(), name='login'),
+    url(r'^login/check/$', views.checklogin.as_view(), name='login_check'),
+    url(r'^log_out/$', views.log_out.as_view(), name='log_out'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^add/$', views.BookCreate.as_view(), name='book_add'),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.BookDelete.as_view(), name='book_delete'),
--- books/views.py
+++ books/views.py
@@ -2,13 +2,19 @@
 from .models import Book
 from django.urls import reverse_lazy
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
 from django.views.generic import  View
 from .forms import UserForm
+from django import  forms
+from django.http import HttpResponse
+
+class first_page(TemplateView):
+    template_name="books/first_page.html"
+
 
 class IndexView(ListView):
     queryset = Book.objects.all()
@@ -34,7 +40,7 @@ class Delete_Success_page(TemplateView):
 
 class UserFormView(View):
     form_class = UserForm
-    template_name = 'books/registration_form.html'
+    template_name = 'books/signup_form.html'
 
     # display blank form
     def get(self, request):
@@ -63,5 +69,28 @@ class UserFormView(View):
         else:
             return render(request, self.template_name, {'form': form})
 
+class login_user(View):
+    form_class = UserForm
+    template_name = 'books/login_form.html'
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
 
+class checklogin(View):
+    form_class = UserForm
+    def post(self, request):
+        form = self.form_class(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect('books:index')
+        else:
+            return HttpResponse("<h1>OOPS!<br>You entered wrong details. Please enter url again.")
+
+class log_out(View):
+    def get(self, request):
+        logout(request)
+        return redirect('books:first_page')
